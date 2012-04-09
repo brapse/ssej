4dff3da10bc64be18fd6b3fba6ec07f543c728b5
Sean Braithwaite <brapse@gmail.com>
2012-04-09 17:03:32 -0400

    Configuration:
      + refactor config concerns into it's own module
      + add support for user config
        - presets and matching files

--- ssej.js
+++ ssej.js
@@ -1,62 +1,11 @@
-var program = require('commander'),
-    LineStream = require('./lib/line_stream').LineStream,
+var LineStream = require('./lib/line_stream').LineStream,
     AggregateFiles = require('./lib/aggregate_files').AggregateFiles;
 
-var extractFunc = function (predicate) {
-    var funcText = 'return ' +  predicate + ';'
-
-    return new(Function)('line', 'group', funcText);
-}
-
-var generateParseFunc = function (parseFunc) {
-    var funcText = 'return ' + parseFunc+ '(line);'
-
-    return new(Function)('line', funcText);
-}
-
-var extractReduce = function (parseFunc) {
-    var funcText = 'return ' + parseFunc+ ';';
-
-    return new(Function)('group', funcText);
-}
-
-
-var identity = function (f) { return f };
-
-program
-  .version('0.0.1')
-  .usage('[options] <map statement> <file ...>')
-  .option('-f, --filter [statement]', 'filter statement', extractFunc, function () { return true})
-  .option('-m, --map [statement]', 'map statement', extractFunc, identity)
-  .option('-c, --count [statement]', 'count [statement]', extractFunc, extractFunc('line'))
-  .option('-g, --group [statement]', 'group by [statement]', extractFunc)
-  .option('-s, --store [statement]', 'specify what to store in groups', extractFunc, identity)
-  .option('-r, --reduce [statement]', 'reduce each group by [statement]', extractReduce, identity)
-  .option('-p, --parser [parseFunc]', 'a parser function, like JSON.parse maybe', generateParseFunc, function (f) { return f})
-  .parse(process.argv);
-
-var determineFiles = function (files) {
-    var notFound = files.filter(function (file) {
-        try {
-            var stat = fs.statSync(file);
-            return true;
-        } catch (e) {
-            return false;
-        }
-    });
-
-    if (notFound.length > 0) {
-        throw new(Error)('Files not found', notFound.join(' '));
-    }
-
-    return files;
-};
-
-var inputFiles = determineFiles(program.args);
+var program = require('./lib/configuration').program;
 
 var inputStream = (function () {
-    if (inputFiles.length > 0) {
-        return new(AggregateFiles)(inputFiles)
+    if (program.inputFiles.length > 0) {
+        return new(AggregateFiles)(program.inputFiles)
     } else {
         process.stdin.resume();
         process.stdin.setEncoding('utf8');
@@ -89,7 +38,7 @@ if (program.group) {
 lineStream.on('data', function (line) {
     var parsed = program.parser(line);
     if (program.filter(parsed)) {
-        if(program.group) {
+        if (program.group) {
             var key = program.group(parsed);
             if (!groups[key]) {
                 groups[key] = []
